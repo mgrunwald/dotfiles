d6b7c652aa0f8735536ec7b84156a4edb388de1b
Markus Grunwald <markus.grunwald@gmx.de>
2010-11-23 10:54:29 +0100

    * Added many settings for damian. Maybe, I'll throw them out again   soon....

--- my/config/emacs-rc-cedet.el
+++ my/config/emacs-rc-cedet.el
@@ -4,13 +4,16 @@
 ;;
 ;; Author: alexott@gmail.com
 ;;
-;; Geklaut und f�r mich angepasst von markus grunwald
+;; Geklaut und für mich angepasst von markus grunwald
 ;;
 
 (load-file "~/.emacs.d/my/lisp/cedet/common/cedet.el")
 
-(semantic-load-enable-excessive-code-helpers)
-;;(semantic-load-enable-semantic-debugging-helpers)
+;; (semantic-load-enable-minimum-features) ;; enables only minimum of necessary features
+;; (semantic-load-enable-code-helpers) ;;enables senator-minor-mode for navigation in buffer, semantic-mru-bookmark-mode for storing positions of visited tags, and semantic-idle-summary-mode, that shows information about tag under point;
+;; (semantic-load-enable-gaudy-code-helpers) ;;enables semantic-stickyfunc-name that displays name of current function in topmost line of buffer, semantic-decoration-mode to decorate tags, using different faces, and semantic-idle-completion-mode for automatic generation of possible names completions, if user stops his work for some time;
+(semantic-load-enable-excessive-code-helpers) ;;enables which-func-mode, that shows name of current function in status line;
+;; (semantic-load-enable-semantic-debugging-helpers) ;;enables several modes, that are useful when you debugging Semantic — displaying of parsing errors, its state, etc.
 
 (setq senator-minor-mode-name "SN")
 (setq semantic-imenu-auto-rebuild-directory-indexes nil)
@@ -36,17 +39,19 @@
 
 ;; customisation of modes
 (defun mg/cedet-hook ()
-  (local-set-key [(control return)] 'semantic-ia-complete-symbol-menu)
-  (local-set-key "\C-c?" 'semantic-ia-complete-symbol)
-  ;;
-  (local-set-key "\C-c>" 'semantic-complete-analyze-inline)
-  (local-set-key "\C-c=" 'semantic-decoration-include-visit)
+  ;; (local-set-key [(control return)] 'semantic-ia-complete-symbol-menu)
+  ;; (local-set-key "\C-c?" 'semantic-ia-complete-symbol)
+  ;; ;;
+  ;; (local-set-key "\C-c>" 'semantic-complete-analyze-inline)
+  ;; (local-set-key "\C-c=" 'semantic-decoration-include-visit)
 
-  (local-set-key "\C-cj" 'semantic-ia-fast-jump)
-  (local-set-key "\C-cq" 'semantic-ia-show-doc)
-  (local-set-key "\C-cs" 'semantic-ia-show-summary)
-  (local-set-key "\C-cp" 'semantic-analyze-proto-impl-toggle)
+  ;; (local-set-key "\C-cj" 'semantic-ia-fast-jump)
+  ;; (local-set-key "\C-cq" 'semantic-ia-show-doc)
+  ;; (local-set-key "\C-cs" 'semantic-ia-show-summary)
+  ;; (local-set-key "\C-cp" 'semantic-analyze-proto-impl-toggle)
+  (semantic-stickyfunc-mode)
   )
+
 ;; (add-hook 'semantic-init-hooks 'mg/cedet-hook)
 (add-hook 'c-mode-common-hook 'mg/cedet-hook)
 (add-hook 'lisp-mode-hook 'mg/cedet-hook)
@@ -60,7 +65,7 @@
   (local-set-key "\C-ct" 'eassist-switch-h-cpp)
   (local-set-key "\C-xt" 'eassist-switch-h-cpp)
   (local-set-key "\C-ce" 'eassist-list-methods)
-  (local-set-key "\C-c\C-r" 'semantic-symref)
+  ;; (local-set-key "\C-c\C-r" 'semantic-symref)
   )
 (add-hook 'c-mode-common-hook 'mg/c-mode-cedet-hook)
 
@@ -91,16 +96,36 @@
 (semantic-add-system-include "/opt/qt/x86/qt3/include" 'c++-mode)
 (semantic-add-system-include "/usr/local/include/pt" 'c++-mode)
 
+;; DSP/BIOS
+( let* (( dsp-base-toolchain "/home/gru/OMAPL137_arm_1_00_00_11")
+        ( dsp-base-sabios   (concat dsp-base-toolchain "/bios_5_33_05" ) )
+        ( dsp-base-compiler (concat dsp-base-toolchain "/cg6x_6_1_9" ) )
+        ( dsp-dsplink (concat dsp-base-toolchain "/dsplink-1_61_03-prebuilt/packages/dsplink") ))
+  (semantic-add-system-include (concat dsp-dsplink "/dsp/inc" ) 'c-mode)
+  (semantic-add-system-include (concat dsp-dsplink "/dsp/inc/DspBios" ) 'c-mode)
+  (semantic-add-system-include (concat dsp-dsplink "/dsp/inc/DspBios/5.XX" ) 'c-mode)
+  (semantic-add-system-include (concat dsp-dsplink "/dsp/inc/DspBios/5.XX/OMAPL1XXGEM" ) 'c-mode)
+  (semantic-add-system-include (concat dsp-dsplink "/dsp/inc/C64XX" ) 'c-mode)
+  (semantic-add-system-include (concat dsp-base-sabios "/packages/ti/bios/include" ) 'c-mode)
+  (semantic-add-system-include (concat dsp-base-compiler "/include" ) 'c-mode)
+  (semantic-add-system-include (concat dsp-base-sabios "/packages/ti/rtdx/include/c6000" ) 'c-mode)
+  (semantic-add-system-include (concat dsp-base-sabios "/packages/ti/psl/include" ) 'c-mode)
+  (semantic-add-system-include (concat dsp-dsplink "/dsp/export/INCLUDE/DspBios/OMAPL1XX" ) 'c-mode)
+  (semantic-add-system-include (concat dsp-base-toolchain "/linuxutils_2_23_01/packages/ti/sdo/linuxutils/cmem/include" ) 'c-mode)
+  )
+
+( let (( damian-firmware-base
+         "/home/gru/projects/damian/trunk/MPC/firmware" ))
+  ( semantic-add-system-include (concat damian-firmware-base "/dao" ) 'c++-mode )
+  ( semantic-add-system-include (concat damian-firmware-base "/setup" ) 'c++-mode )
+  ( semantic-add-system-include (concat damian-firmware-base "/measurement" ) 'c++-mode )
+  ( semantic-add-system-include (concat damian-firmware-base "/measurement/confighandling" ) 'c++-mode )
+  ( semantic-add-system-include (concat damian-firmware-base "/measurement/devicecom/" ) 'c++-mode )
+  ( semantic-add-system-include (concat damian-firmware-base "/measurement/resulthandling/" ) 'c++-mode )
+)
 
 (add-to-list 'auto-mode-alist '("/opt/qt/x86/qt3/include" . c++-mode))
 (add-to-list 'semantic-lex-c-preprocessor-symbol-file "/opt/qt/x86/qt3/include/qconfig.h")
-
-;;(semantic-add-system-include "/exp/include" 'c-mode)
-
-;; (setq boost-base-directory "~/exp/include/boost-1_40/")
-
-;; (add-to-list 'semantic-lex-c-preprocessor-symbol-file
-;;              (concat boost-base-directory "/boost/config.hpp"))
 
 ;;
 (global-semantic-idle-tag-highlight-mode 1)
@@ -139,6 +164,61 @@
 ;;                                               )
 ;;                             ))
 
+(setq damian-dsp-project
+      (ede-cpp-root-project "damian-dsp"
+                            :file "~/projects/damian/trunk/DSP/pt/measurement_core/main.c"
+                            :local-variables (list
+                                              (cons 'compile-command
+                                                    'mg/gen-std-compile-string )
+                                              )
+                            )
+      )
+
+(setq damian-loaddsp-project
+      (ede-cpp-root-project "damian-loaddsp"
+                            :file "~/projects/damian/trunk/DSP/pt/tools/loaddsp/arm_main.c"
+                            :local-variables (list
+                                              (cons 'compile-command
+                                                    'mg/gen-std-compile-string )
+                                              )
+                            )
+      )
+
+(setq damian-loader-project
+      (ede-cpp-root-project "damian-loader"
+                            :file "~/projects/damian/trunk/DSP/pt/tools/loader/loader.c"
+                            :local-variables (list
+                                              (cons 'compile-command
+                                                    'mg/gen-std-compile-string )
+                                              )
+                            )
+      )
+
+(setq damian-firmware-project
+      (ede-cpp-root-project "damian-firmware-project"
+                            :file "~/projects/damian/trunk/MPC/firmware/DAMIAN.pro"
+                            :local-variables (list
+                                              (cons 'compile-command
+                                                    'mg/gen-std-compile-string
+                                                    )
+                                              (cons 'indent-tabs-mode t )
+                                              )
+                            )
+      )
+
+(setq damian-test-project
+      (ede-cpp-root-project "damian-test-project"
+                            :file "~/projects/damian/trunk/MPC/firmware/TESTING/AutomaticTests/AutomaticTests.pro"
+                            :local-variables (list
+                                              (cons 'compile-command
+                                                    'mg/gen-plain-make-compile-string
+                                                    )
+                                              (cons 'indent-tabs-mode t )
+                                              )
+                            )
+      )
+
+
 ;; my functions for EDE
 (defun mg/ede-get-local-var (fname var)
   "fetch given variable var from :local-variables of project of file fname"
@@ -157,29 +237,41 @@
 ;; (setq compilation-scroll-output t)
 ;; (setq mode-compile-always-save-buffer-p t)
 
-;; (defun mg/compile ()
-;;   "Saves all unsaved buffers, and runs 'compile'."
-;;   (interactive)
-;;   (save-some-buffers t)
-;;   (let* ((r (mg/ede-get-local-var
-;;              (or (buffer-file-name (current-buffer)) default-directory)
-;;              'compile-command))
-;;          (cmd (if (functionp r) (funcall r) r)))
-;; ;;    (message "AA: %s" cmd)
-;;     (set (make-local-variable 'compile-command) (or cmd compile-command))
-;;     (compile compile-command)))
+(defun mg/compile (arg)
+  "Saves all unsaved buffers, and runs 'compile'."
+  (interactive "P" )
+  (save-some-buffers t)
+  (let* ((r (mg/ede-get-local-var
+             (or (buffer-file-name (current-buffer)) default-directory)
+             'compile-command))
+         (cmd (if (functionp r) (funcall r) r)))
+;;    (message "AA: %s" cmd)
+    (set (make-local-variable 'compile-command) (or cmd compile-command))
+    (when arg (setq compile-command (read-from-minibuffer "Compile command: "
+					   compile-command nil nil
+					   '(compile-history . 1)))
+          )
+    (compile compile-command)))
 
-;; (global-set-key [f9] 'mg/compile)
+(global-set-key [f9] 'mg/compile)
 
-;; ;;
-;; (defun mg/gen-std-compile-string ()
-;;   "Generates compile string for compiling CMake project in debug mode"
-;;   (let* ((current-dir (file-name-directory
-;;                        (or (buffer-file-name (current-buffer)) default-directory)))
-;;          (prj (ede-current-project current-dir))
-;;          (root-dir (ede-project-root-directory prj))
-;;          )
-;;     (concat "cd " root-dir "; make -j2")))
+;;
+(defun mg/gen-std-compile-string ()
+  "Generates compile string for compiling a project in debug mode"
+  (let* ((current-dir (file-name-directory
+                       (or (buffer-file-name (current-buffer)) default-directory)))
+         (prj (ede-current-project current-dir))
+         (root-dir (ede-project-root-directory prj))
+         )
+    (concat "cd " root-dir "; make DEBUG=1")
+    )
+  )
+
+(defun mg/gen-plain-make-compile-string ()
+  "Generates compile string for compiling a project in debug mode"
+    (concat "make"))
+
+;;(mg/gen-std-compile-string)
 
 ;; ;;
 ;; (defun mg/gen-cmake-debug-compile-string ()
@@ -196,14 +288,17 @@
 ;; ;;    (concat "cd " root-dir "Debug/" subdir "; make -j3")))
 
 ;; Example, Qt customization
-;; (setq qt4-base-dir "/usr/include/qt4")
-;; (setq qt4-gui-dir (concat qt4-base-dir "/QtGui"))
-;; (semantic-add-system-include qt4-base-dir 'c++-mode)
-;; (semantic-add-system-include qt4-gui-dir 'c++-mode)
-;; (add-to-list 'auto-mode-alist (cons qt4-base-dir 'c++-mode))
-;; (add-to-list 'semantic-lex-c-preprocessor-symbol-file (concat qt4-base-dir "/Qt/qconfig.h"))
-;; (add-to-list 'semantic-lex-c-preprocessor-symbol-file (concat qt4-base-dir "/Qt/qconfig-large.h"))
-;; (add-to-list 'semantic-lex-c-preprocessor-symbol-file (concat qt4-base-dir "/Qt/qglobal.h"))
+(setq qt4-base-dir "/usr/include/qt4")
+(setq qt4-gui-dir (concat qt4-base-dir "/QtGui"))
+(setq qt4-core-dir (concat qt4-base-dir "/QtCore"))
+(semantic-add-system-include qt4-base-dir 'c++-mode)
+(semantic-add-system-include qt4-gui-dir 'c++-mode)
+(semantic-add-system-include qt4-core-dir 'c++-mode)
+
+(add-to-list 'auto-mode-alist (cons qt4-base-dir 'c++-mode))
+(add-to-list 'semantic-lex-c-preprocessor-symbol-file (concat qt4-base-dir "/Qt/qconfig.h"))
+(add-to-list 'semantic-lex-c-preprocessor-symbol-file (concat qt4-base-dir "/Qt/qconfig-large.h"))
+(add-to-list 'semantic-lex-c-preprocessor-symbol-file (concat qt4-base-dir "/Qt/qglobal.h"))
 
 ;;; emacs-rc-cedet.el ends here
 
