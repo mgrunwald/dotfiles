5f46263b0f041d426e9b929c34537f309d9089a2
Markus Grunwald <markus.grunwald@gmx.de>
2011-10-07 15:31:09 +0200

    * mg-auto-insert-style * pt style c sections

--- my/config/pt-auto-insert.el
+++ my/config/pt-auto-insert.el
@@ -1,6 +1,8 @@
 ;;; pt-auto-insert.el --- templates for auto-insert.el
 ;; Copyright (C) 2004 Markus Grunwald
 
+(defvar mg-auto-insert-style nil
+  "*Style for auto insertion in new files or other documentation" )
 
 ;; This is the copyright holder
 (if (boundp 'my-copyright-holder)
@@ -8,38 +10,166 @@
   (setq auto-insert-copyright (user-full-name)))
 
 
+(defun pt-make-file-header()
+  "Inserts a Makefile header"
+  (interactive)
+  ( if (boundp 'mg-auto-insert-style )
+      ( case mg-auto-insert-style
+        ( (damian dafit)
+          ( insert
+            " # @file "(file-name-nondirectory buffer-file-name)"\n"
+            " # This file may not be reproduced, disclosed or used in whole\n"
+            " # or in part without the express written permission of\n"
+            " # Pruftechnik Condition Monitoring GmbH.\n"
+            " # \n"
+            " # (c) 2000 - "( format-time-string "%Y" )" by Pruftechnik Condition Monitoring GmbH\n\n"
+            )
+          )
+        )
+    )
+  )
+
 (defun pt-c-file-header()
   "Inserts a c/c++ file header"
-  ( insert
-    "/** <!------------------------------------------------------------------------>\n"
-    " * @file "(file-name-nondirectory buffer-file-name)"\n"
-    " * This file may not be reproduced, disclosed or used in whole\n"
-    " * or in part without the express written permission of\n"
-    " * Pruftechnik Condition Monitoring GmbH.\n"
-    " * \n"
-    " * (c) 2000 - "( format-time-string "%Y" )" by Pruftechnik AG\n"
-    " <!------------------------------------------------------------------------> */\n\n" )
+  ( if (boundp 'mg-auto-insert-style )
+      ( case mg-auto-insert-style
+        ( dafit
+          ( insert
+            "/** <!------------------------------------------------------------------------>\n"
+            " * @file "(file-name-nondirectory buffer-file-name)"\n"
+            " * This file may not be reproduced, disclosed or used in whole\n"
+            " * or in part without the express written permission of\n"
+            " * Pruftechnik Condition Monitoring GmbH.\n"
+            " * \n"
+            " * (c) 2000 - "( format-time-string "%Y" )" by Pruftechnik AG\n"
+            " <!------------------------------------------------------------------------> */\n\n"
+            )
+          )
+        ( damian
+          ( insert
+            "/** <!-------------------------------------------------------------------------------------------->\n"
+            " * @file "(file-name-nondirectory buffer-file-name)"\n"
+            " * This file may not be reproduced, disclosed or used in whole\n"
+            " * or in part without the express written permission of\n"
+            " * Pruftechnik Condition Monitoring GmbH.\n"
+            " * \n"
+            " * (c) 2000 - "( format-time-string "%Y" )" by Pruftechnik Condition Monitoring GmbH\n"
+            " <!--------------------------------------------------------------------------------------------> */ \n"
+            )
+          )
+        (otherwise (message "Meh."))
+        )
+    )
   )
 
 (defun pt-c-class-header()
   "Inserts a c/c++ class header"
-  ( insert
-    "/** <!------------------------------------------------------------------------>\n"
-    " * Description:\n"
-    " * \n"
-    " *********************************************************************************\n"
-    " * @class    "(file-name-nondirectory (file-name-sans-extension buffer-file-name))"\n"
-    " * <br>\n"
-    " * System    Dafit <br>\n"
-    " *********************************************************************************\n"
-    " * @author   "(user-full-name) "\n"
-    " * @since    "( format-time-string "%Y-%m-%d" )"\n"
-    " * \n"
-    " * @version  1.0 " ( format-time-string "%Y-%m-%d" ) " created by "(user-full-name) "\n"
-    " <!------------------------------------------------------------------------> */ \n" )
+  ( if (boundp 'mg-auto-insert-style )
+      ( case mg-auto-insert-style
+        ( dafit
+          ( insert
+            "/** <!------------------------------------------------------------------------>\n"
+            " * Description:\n"
+            " * \n"
+            " *********************************************************************************\n"
+            " * @class    "(file-name-nondirectory (file-name-sans-extension buffer-file-name))"\n"
+            " * <br>\n"
+            " * System    Dafit <br>\n"
+            " *********************************************************************************\n"
+            " * @author   "(user-full-name) "\n"
+            " * @since    "( format-time-string "%Y-%m-%d" )"\n"
+            " * \n"
+            " * @version  1.0 " ( format-time-string "%Y-%m-%d" ) " created by "(user-full-name) "\n"
+            " <!------------------------------------------------------------------------> */ \n"
+            )
+          ) ;; dafit
+        ( damian
+          ( insert
+            "/** <!-------------------------------------------------------------------------------------------->\n"
+            " * \n"
+            " * \n"
+            " * @system  Damian\n"
+            " * @author   "(user-full-name) "\n"
+            " * @since    "( format-time-string "%Y-%m-%d" )"\n"
+            " <!--------------------------------------------------------------------------------------------> */ \n"
+            )
+          )
+        ( otherwise
+          ( insert
+            "/** <!------------------------------------------------------------------------>\n"
+            " * \n"
+            " * \n"
+            " * @author   "(user-full-name) "\n"
+            " * @since    "( format-time-string "%Y-%m-%d" )"\n"
+            " <!------------------------------------------------------------------------> */ \n"
+            )
+          )
+        ) ;; case
+    ( insert
+            "/** <!------------------------------------------------------------------------>\n"
+            " * \n"
+            " <!------------------------------------------------------------------------> */ \n" )
+    )
   )
 
-
+(defun pt-insert-c-sections()
+  "Inserts c section comments"
+  (interactive)
+  (insert
+   "/*##############################################################################\n"
+   "* INCLUDES\n"
+   "*/\n"
+   "#include <getopt.h>\n"
+   "#include <stdio.h>\n"
+   "#include <stdlib.h>\n"
+   "#include <string.h>\n"
+   "#include <unistd.h>\n"
+   "\n"
+   "\n"
+   "/*##############################################################################\n"
+   "* DEFINES\n"
+   "*/\n"
+   "\n"
+   "\n"
+   "/*##############################################################################\n"
+   "* TYPEDEFS\n"
+   "*/\n"
+   "\n"
+   "\n"
+   "/*##############################################################################\n"
+   "* GLOBALS\n"
+   "*/\n"
+   "\n"
+   "\n"
+   "/*##############################################################################\n"
+   "* LOCALS\n"
+   "*/\n"
+   "static const char CVS_ID[] = \"$Id$\";\n"
+   "static const char BUILD_TIME[] = __DATE__ \" \" __TIME__;\n"
+   "\n"
+   "\n"
+   "/*##############################################################################\n"
+   "* MACROS\n"
+   "*/\n"
+   "\n"
+   "\n"
+   "/*##############################################################################\n"
+   "* FORWARD DECLARATIONS\n"
+   "*/\n"
+   "\n"
+   "\n"
+   "/*##############################################################################\n"
+   "* GLOBAL FUNCTIONS\n"
+   "*/\n"
+   "\n"
+   "\n"
+   "/*##############################################################################\n"
+   "* LOCAL FUNCTIONS\n"
+   "*/\n"
+   "\n"
+   "\n"
+   )
+  )
 
 ;; And here comes The List
 (setq auto-insert-alist
@@ -143,17 +273,7 @@
         (("\\.c$" . "C Program")
          nil
          ( pt-c-file-header )
-         "// --- includes --- \n"
-         "#include <getopt.h>\n"
-         "#include <stdio.h>\n"
-         "#include <stdlib.h>\n"
-         "#include <string.h>\n"
-         "#include <unistd.h>\n"
-         "\n\n"
-         "static const char CVS_ID[] = \"$Id$\";\n"
-         "static const char BUILD_TIME[] = __DATE__ \" \" __TIME__;\n"
-         "\n"
-         "\n"
+         ( pt-insert-c-sections )
          "/** **************************************************************************\n"
          " * DESCRIPTION: Output of help to cerr and exit\n"
          " * ARGUMENTS:   program name\n"
