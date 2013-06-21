b95b1cf8b1a6196a12a9a0f44d2226284ab7ac38
Wesley Werner <wez@darknet.co.za>
2013-06-21 23:57:04 +0200

    dunst config

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,4 +2,9 @@
 # boot strap all our links
 # you may need to place this in .bashrc: source ~/.bash_userrc
 
+# user bashrc
 ln -s ~/dotfiles/.bash_userrc ~/
+
+# dunst
+mkdir ~/.config/dunst
+ln -s ~/dotfiles/.config/dunst/dunstrc ~/.config/dunst/
