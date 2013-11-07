7e793208ddd9f862226c89abb8e3c9542b33f9a1
Markus Grunwald <markus.grunwald@pruftechnik.com>
2013-11-07 12:17:21 +0100

    Calc ( C-x * * ) configuration

---
+++ links/.emacs.d/calc.el
@@ -0,0 +1,7 @@
+
+;;; Mode settings stored by Calc on Fri Sep 27 10:52:46 2013
+(setq calc-window-height 22)
+(setq calc-complex-format 'i)
+(setq calc-date-format '(YYYY "-" MM "-" DD (" " hh ":" mm ":" ss " ") Www))
+;;; End of mode settings
+
