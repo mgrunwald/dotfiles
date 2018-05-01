85e87e9d579e9efc32fc70cea0304041b6c4db0c
Markus Grunwald <markus.grunwald@gmx.de>
2018-05-01 20:03:20 +0200

    Set default font.

--- links/.emacs.d/custom.el
+++ links/.emacs.d/custom.el
@@ -588,7 +588,6 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :stipple nil :background "#3f3f3f" :foreground "#dcdccc" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 102 :width normal :foundry "normal" :family "DejaVu Sans Mono"))))
  '(grep-edit-face ((((class color) (background light)) (:background "LightGreen" :weight bold))))
  '(highlight ((((class color) (background light)) (:background "gray80"))))
  '(hl-line ((t nil)))
